59efe59ae06ff4e340dd11ffe6de792e7f839b7a
Corey <corey.amano@gmail.com>
2021-06-25 12:45:58 -0700

    solve problems 4 and 5

--- arrays_select.rb
+++ arrays_select.rb
@@ -41,8 +41,28 @@ p over_fives
 #  4. Start with an array of numbers and create a new array with only the even numbers.
 #     For example, [2, 4, 5, 1, 8, 9, 7] becomes [2, 4, 8].
 
+nums = [2, 4, 5, 1, 8, 9, 7]
+evens = []
+
+nums.each do |num|
+  if num % 2 == 0
+    evens << num
+  end
+end
+p evens
+
 #  5. Start with an array of strings and create a new array with only the strings shorter than 4 letters.
 #     For example, ["a", "man", "a", "plan", "a", "canal", "panama"] becomes ["a", "man", "a", "a"].
+
+words = ["a", "man", "a", "plan", "a", "canal", "panama"]
+short_words = []
+
+words.each do |word|
+  if word.length < 4
+    short_words << word
+  end
+end
+p short_words
 
 #  6. Start with an array of hashes and create a new array with only the hashes with names shorter than 6 letters (from the :name key).
 #     For example, [{name: "chair", price: 100}, {name: "pencil", price: 1}, {name: "book", price: 4}] becomes [{name: "chair", price: 100}, {name: "book", price: 4}].
